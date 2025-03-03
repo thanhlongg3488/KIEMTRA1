2c1f350e70c2c2cdcc5cb7891442ef9f34b51f9e
thanhlong3488 <longntl037@gmail.com>
2025-03-03 09:21:35 +0700

    Thêm Label UserName, textbox: txtUserName

--- KT1/Form1.Designer.cs
+++ KT1/Form1.Designer.cs
@@ -28,12 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            txtUserName = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(96, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 25);
+            label1.TabIndex = 0;
+            label1.Text = "UserName";
+            // 
+            // txtUserName
+            // 
+            txtUserName.Location = new Point(238, 52);
+            txtUserName.Name = "txtUserName";
+            txtUserName.Size = new Size(331, 31);
+            txtUserName.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtUserName);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtUserName;
     }
 }
